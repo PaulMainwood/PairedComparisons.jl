78e31a14682087d8c78f95711e2170ca230d07d1
Paul Mainwood <paul.mainwood@gmail.com>
2020-05-06 22:14:01 +0100

    Add oneaheads back in.

--- src/PairedComparisons.jl
+++ src/PairedComparisons.jl
@@ -7,6 +7,7 @@ include("elo.jl")
 include("glicko.jl")
 include("bradleyterry.jl")
 include("WHR.jl")
+include("oneahead.jl")
 
 export Elo, Glicko, BradleyTerry, BT, WHR, fit!, one_ahead!, predict, iterate!, add_games!, loglikelihood
 
