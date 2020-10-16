07daaef6a1aff07c57fec99600306427d59832bb
dzonimn <i.m.jonc@gmail.com>
2020-10-16 21:17:28 +0800

    Fix export method name

--- src/PairedComparisons.jl
+++ src/PairedComparisons.jl
@@ -9,6 +9,6 @@ include("bradleyterry.jl")
 include("WHR.jl")
 include("oneahead.jl")
 
-export Elo, Glicko, BradleyTerry, BT, WHR, fit!, one_ahead!, predict, iterate!, add_games!, log_likelihood, brier, brierss, actual_scores, log_loss, rating, filter_played_before, display_ratings, change_in_surface, jumble, dupe_for_rating, predict_or_missing, rating, mean_logitnormal, uncertainty, unscrambletuples
+export Elo, Glicko, BradleyTerry, BT, WHR, fit!, one_ahead!, predict, iterate!, add_games!, loglikelihood, brier, brierss, actual_scores, log_loss, rating, filter_played_before, display_ratings, change_in_surface, jumble, dupe_for_rating, predict_or_missing, rating, mean_logitnormal, uncertainty, unscrambletuples
 
 end # module
